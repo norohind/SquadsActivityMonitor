7965891c74a5b2bc706f369f17a0879b7a426641
norohind <60548839+norohind@users.noreply.github.com>
2024-05-01 21:59:39 +0300

    WRYRHook.py: Add R0SD

--- hooks/WRYRHook.py
+++ hooks/WRYRHook.py
@@ -32,7 +32,7 @@ EMBED_TEMPLATE = {
     ]
 }
 # as an idea: notifications as a service
-TRACKED_TAGS = ['COFF', 'PLCN', 'CQCD', 'RMLK', 'AXIN', '7725', 'MYCF', 'GXIN', 'L4ND', 'NEWP', 'CCLS', 'BAAS', 'MAXA', 'EGPU', 'LLPC', 'DAOS', 'J3DI', 'ENEX', 'G911', 'IHCF', 'EDXN', 'HLLN', 'NTNS', 'VAT9']
+TRACKED_TAGS = ['COFF', 'PLCN', 'CQCD', 'RMLK', 'AXIN', '7725', 'MYCF', 'GXIN', 'L4ND', 'NEWP', 'CCLS', 'BAAS', 'MAXA', 'EGPU', 'LLPC', 'DAOS', 'J3DI', 'ENEX', 'G911', 'IHCF', 'EDXN', 'HLLN', 'NTNS', 'VAT9', 'R0SD']
 
 
 def notify_discord(message: dict) -> None:
