61ec047e625d537d235ca5562be8293bbbac1db8
norohind <60548839+norohind@users.noreply.github.com>
2026-01-19 21:01:11 +0300

    Update WRYRHook.py

--- hooks/WRYRHook.py
+++ hooks/WRYRHook.py
@@ -32,7 +32,7 @@ EMBED_TEMPLATE = {
     ]
 }
 # as an idea: notifications as a service
-TRACKED_TAGS = ['COFF', 'PLCN', 'CQCD', 'RMLK', 'AXIN', '7725', 'MYCF', 'GXIN', 'L4ND', 'NEWP', 'CCLS', 'BAAS', 'MAXA', 'EGPU', 'LLPC', 'DAOS', 'J3DI', 'ENEX', 'G911', 'IHCF', 'EDXN', 'HLLN', 'NTNS', 'VAT9', 'R0SD', 'RXLA', 'CZ88', 'ECH0', 'GTBE', 'ROEL', 'GOB5', 'MAKH', 'UKRS', 'ASOG', 'STFR', 'BS13', 'RDTK', 'HIDB', 'IBAS']
+TRACKED_TAGS = ['COFF', 'PLCN', 'CQCD', 'RMLK', 'AXIN', '7725', 'MYCF', 'GXIN', 'L4ND', 'NEWP', 'CCLS', 'BAAS', 'MAXA', 'EGPU', 'LLPC', 'DAOS', 'J3DI', 'ENEX', 'G911', 'IHCF', 'EDXN', 'HLLN', 'NTNS', 'VAT9', 'R0SD', 'RXLA', 'CZ88', 'ECH0', 'GTBE', 'ROEL', 'GOB5', 'MAKH', 'UKRS', 'ASOG', 'STFR', 'BS13', 'RDTK', 'HIDB', 'IBAS', 'QCO7']
 
 
 def notify_discord(message: dict) -> None:
@@ -79,6 +79,7 @@ class WRYRHook(Hook):
 
 def setup(hs: HookSystem):
     hs.add_update_hook(WRYRHook())
+
 
 
 
